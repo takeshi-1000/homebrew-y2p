56c12d96a0e399f2c413cfb76649b0d66dd6fea3
takeshi-1000 <stubgurssy@gmail.com>
2023-05-15 09:03:55 +0900

    バイナリに変える

--- y2p.rb
+++ y2p.rb
@@ -1,14 +1,11 @@
 class Y2p < Formula
   desc "yaml to screen transion diagram"
   homepage "https://medium.com/@takeshi-1000/a-program-to-generate-screen-transition-diagrams-from-yaml-311c54475b3a"
-  url "https://github.com/takeshi-1000/y2p.git"
+  url "https://github.com/takeshi-1000/y2p/releases/download/v1.0/y2p"
   sha256 "d147cede1eb653386640f5ef3a333dc9381622d0c8d6b6ab7d7ebe7b5ac3e063"
   license "MIT"
-  
-  depends_on "swift" => :build
-  
+    
   def install
-      system "swift", "build", "--configuration", "release", "--disable-sandbox"
-      bin.install ".build/release/y2p"
+      bin.install "y2p"
   end
 end
