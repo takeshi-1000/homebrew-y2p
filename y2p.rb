0c736c767dc5bfe26f1b41e9c4a007ea0203b950
takeshi-1000 <stubgurssy@gmail.com>
2023-05-15 13:39:54 +0900

    ハッシュ値が違うようなので更新

--- y2p.rb
+++ y2p.rb
@@ -2,7 +2,7 @@ class Y2p < Formula
   desc "yaml to screen transion diagram"
   homepage "https://medium.com/@takeshi-1000/a-program-to-generate-screen-transition-diagrams-from-yaml-311c54475b3a"
   url "https://github.com/takeshi-1000/y2p/archive/refs/tags/v1.0.tar.gz"
-  sha256 "d147cede1eb653386640f5ef3a333dc9381622d0c8d6b6ab7d7ebe7b5ac3e063"
+  sha256 "a1e4b8c9188b72fa5363dd1653fc0762acdc35b6b9cd8cd979f7d8fbed57d505"
   license "MIT"
   
   depends_on "swift" => :build
